c28280d7d695c41808a26c195ce6c0898fbcbdc5
EldinSalja <eldin.salja@gmail.com>
2024-12-26 20:54:08 -0600

    Project Creation

---
+++ script.js
@@ -0,0 +1,28 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbol = "!@#$%^&*()_+=";
+
+const allChars = upperCase + lowerCase + numbers + symbol;
+
+function createPassword() {
+  let password = "";
+  password += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
+  password += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length));
+  password += numbers.charAt(Math.floor(Math.random() * numbers.length));
+  password += symbol.charAt(Math.floor(Math.random() * symbol.length));
+
+  while(length>password.length){
+    password += allChars[Math.floor(Math.random() * symbol.length)];
+  }
+  passwordBox.value = password;
+}
+
+function copyPassword() {
+  passwordBox.select();
+  document.execCommand("copy");
+  alert("Password copied to clipboard");
+}
